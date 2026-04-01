b647cd365ca9c4b53714b2dadf7e27d8de63b5ea
alericci90 <123756408+alericci90@users.noreply.github.com>
2026-04-01 09:48:14 +0000

    add secrets

--- azure_llm.py
+++ azure_llm.py
@@ -1,12 +1,12 @@
 import os
 from openai import AzureOpenAI
 
-endpoint = "https://aleri-mljnd89q-eastus2.cognitiveservices.azure.com/"
 model_name = "gpt-4o-mini"
 deployment = "gpt-4o-mini"
 
-subscription_key = os.getenv("AZURE_OPENAI_KEY")
-api_version = "2024-12-01-preview"
+endpoint = os.getenv("AZURE_OPENAI_ENDPOINT")
+subscription_key = os.getenv("AZURE_OPENAI_API_KEY")
+api_version = os.getenv("AZURE_OPENAI_API_VERSION")
 
 client = AzureOpenAI(
     api_version=api_version,
